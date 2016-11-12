97ff640d566399407f5da16e3eb7bf7acef52bda
marklabenski <mark.labenski@gmail.com>
2016-11-12 13:23:02 +0100

    add initial workflow and package files

---
+++ gulpfile.babel.js
@@ -0,0 +1,65 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import mocha from 'gulp-mocha';
+import gutil from 'gulp-util';
+import webpack from 'webpack';
+import webpackConfig from './webpack.config.babel';
+import ghPages from 'gulp-gh-pages';
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('default', ['webpack', 'copy']);
+
+gulp.task('babel', () => {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('target'));
+});
+
+gulp.task('copy', function () {
+  gulp
+     .src('src/assets/**/*')
+     .pipe(gulp.dest('dist/assets'));
+  gulp
+    .src('src/styles/main.css')
+    .pipe(gulp.dest('dist'));
+  return gulp.src('src/*.html')
+      .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', ['babel'], () => {
+  return gulp.src('test/*.js')
+    .pipe(mocha())
+    .on('error', () => {
+      gulp.emit('end');
+    });
+});
+
+gulp.task('watch-test', () => {
+  return gulp.watch(['javascript/**', 'test/**'], ['test']);
+});
+
+gulp.task('watch', () => {
+  return gulp.watch(['src/**'], ['default']);
+});
+
+gulp.task('webpack', ['test'], function(callback) {
+  var myConfig = Object.create(webpackConfig);
+  myConfig.plugins = [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin()
+  ];
+
+  // run webpack
+  webpack(myConfig, function(err, stats) {
+    if (err) throw new gutil.PluginError('webpack', err);
+    gutil.log('[webpack]', stats.toString({
+      colors: true,
+      progress: true
+    }));
+    callback();
+  });
+});
